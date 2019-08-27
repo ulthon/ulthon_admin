32e702a32f10f9060da3763cde399a7b61129c1d
augushong <augushong@163.com>
2019-08-27 13:34:31 +0800

    封装上传文件管理

---
+++ app/UploadFiles.php
@@ -0,0 +1,25 @@
+<?php
+namespace app;
+
+class UploadFiles  
+{
+    public static function create()
+    {
+        
+    }
+
+    public static function use()
+    {
+        
+    }
+
+    public static function delete()
+    {
+        
+    }
+
+    public static function clear()
+    {
+        
+    }
+}
