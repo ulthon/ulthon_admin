e3927cc6d2f6dfa8d97d38632598bb008163e556
augushong <augushong@163.com>
2019-08-28 13:37:00 +0800

    封装上传文件管理类;

--- app/UploadFiles.php
+++ app/UploadFiles.php
@@ -1,21 +1,29 @@
 <?php
 namespace app;
 
+use app\model\UploadFiles as AppUploadFiles;
+
 class UploadFiles  
 {
-    public static function create()
+    public static function create($data,$allowFiled = [],$replace = false)
     {
-        
+        return AppUploadFiles::create($data,$allowFiled,$replace);
     }
 
-    public static function use()
+    public static function use($save_name)
     {
-        
+        return AppUploadFiles::where('save_name',$save_name)->update([
+            'used_time'=>time(),
+            'status'=>1
+        ]);
     }
 
-    public static function delete()
+    public static function delete($save_name)
     {
-        
+        return AppUploadFiles::where('save_name',$save_name)->update([
+            'delete_time'=>time(),
+            'status'=>2
+        ]);
     }
 
     public static function clear()
