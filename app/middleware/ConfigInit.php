ef7a0dbfdede7fb8bc43ff73084058c92b095f5a
augushong <augushong@192.168.0.107>
2019-10-10 22:17:32 +0800

    修改问题

--- app/middleware/ConfigInit.php
+++ app/middleware/ConfigInit.php
@@ -11,25 +11,21 @@ class ConfigInit
     public function handle($request, \Closure $next)
     {
 
-        $tables = @Db::query("show tables");
-        if(!empty($tables)){
+        //设置存储
+        $filesystem_config = Config::get('filesystem');
+        Arr::set($filesystem_config,'default','public');
 
-            //设置存储
-            $filesystem_config = Config::get('filesystem');
-            Arr::set($filesystem_config,'default','public');
-    
-            Config::set($filesystem_config, 'filesystem');
-                
-            // 社微信开放平台
+        Config::set($filesystem_config, 'filesystem');
+            
+        // 社微信开放平台
+        // $wx_open_app = [];
+        // $wx_open_app = Arr::add($wx_open_app,'app_id',get_system_config('wx_open_app_id'));
+        // $wx_open_app = Arr::add($wx_open_app,'secret',get_system_config('wx_open_app_secret'));
+        // $wx_open_app = Arr::add($wx_open_app,'token',get_system_config('wx_open_app_token'));
+        // $wx_open_app = Arr::add($wx_open_app,'aes_key',get_system_config('wx_open_app_aes_key'));
 
-            $wx_open_app = [];
-            $wx_open_app = Arr::add($wx_open_app,'app_id',get_system_config('wx_open_app_id'));
-            $wx_open_app = Arr::add($wx_open_app,'secret',get_system_config('wx_open_app_secret'));
-            $wx_open_app = Arr::add($wx_open_app,'token',get_system_config('wx_open_app_token'));
-            $wx_open_app = Arr::add($wx_open_app,'aes_key',get_system_config('wx_open_app_aes_key'));
+        // Config::set($wx_open_app,'wx_open_app');
 
-            Config::set($wx_open_app,'wx_open_app');
-        }
 
         return $next($request);
     }
--- config/database.php
+++ config/database.php
@@ -37,7 +37,7 @@ return [
             // 数据库编码默认采用utf8
             'charset'           => Env::get('database.charset', 'utf8'),
             // 数据库表前缀
-            'prefix'            => Env::get('database.prefix', ''),
+            'prefix'            => Env::get('database.prefix', 'ul_'),
 
             // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
             'deploy'            => 0,
@@ -77,7 +77,7 @@ return [
             // 数据库编码默认采用utf8
             'charset'         => Env::get('database.charset', 'utf8'),
             // 数据库表前缀
-            'prefix'          => Env::get('database.prefix', ''),
+            'prefix'          => Env::get('database.prefix', 'ul_'),
             // 数据库调试模式
             'debug'           => Env::get('database.debug', true),
             // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
