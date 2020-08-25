d7b226c66d54e87f31776cbb2aecdbb2258f85fa
augushong <augushong@163.com>
2020-08-25 22:55:11 +0800

    修复分类编辑传参,去掉无用模板配置

--- config/view_type.php
+++ config/view_type.php
@@ -3,12 +3,6 @@
 return [
   'category' => [
     '' => '普通:',
-
-    '_product' => '产品:_product',
-    '_case' => '案例:_case',
-    '_about_1' => '关于_图块:_about_1',
-    '_about_2' => '关于_招聘:_about_2',
-    '_about_3' => '关于_时间轴:_about_3',
   ],
   'post' => [
     '' => '普通:',
