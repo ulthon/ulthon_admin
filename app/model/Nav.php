960c21ccbe9b91178db484e6bddee83cfaef9a65
augushong <augushong@163.com>
2020-11-19 15:40:17 +0800

    给Nav新增快捷方法

--- app/model/Nav.php
+++ app/model/Nav.php
@@ -12,18 +12,29 @@ use think\Model;
 class Nav extends Model
 {
 
+  protected static $listNav = [];
+
   public static $statusName = [
-    0=>'不显示',
-    1=>'显示'
+    0 => '不显示',
+    1 => '显示'
   ];
   //
   public function getImgAttr($value)
   {
-    return get_source_link($value,'/static/images/noimg.png');
+    return get_source_link($value, '/static/images/noimg.png');
   }
 
   public function getStatusNameAttr()
   {
     return self::$statusName[$this->getData('status')];
+  }
+
+  public static function getListNav($type = 1)
+  {
+
+    if (!isset(self::$listNav[$type])) {
+      self::$listNav[$type] =  Nav::where('type', $type)->where('status', 1)->order('sort asc')->select();
+    }
+    return self::$listNav[$type];
   }
 }
