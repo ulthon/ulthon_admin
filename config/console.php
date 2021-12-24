e29608a85e68873692b019fe54f92b61d6d5e9e1
augushong <augushong@163.com>
2021-12-24 22:57:45 +0800

    完成命令行的安装方式;

--- config/console.php
+++ config/console.php
@@ -4,6 +4,7 @@
 // +----------------------------------------------------------------------
 
 use app\common\command\admin\ResetPassword;
+use app\common\command\Install;
 
 return [
     // 指令定义
@@ -11,6 +12,7 @@ return [
         'curd'      => 'app\common\command\Curd',
         'node'      => 'app\common\command\Node',
         'OssStatic' => 'app\common\command\OssStatic',
-        ResetPassword::class
+        ResetPassword::class,
+        Install::class
     ],
 ];
