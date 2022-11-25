e6bbdc02975d82af30c03fbb5484b49fce669b97
augushong <augushong@ulthon.com>
2022-11-25 17:08:25 +0800

    完善打包，解析方法不混淆

--- app/common/tools/phpparser/NodeFakeVarVisitorTools.php
+++ app/common/tools/phpparser/NodeFakeVarVisitorTools.php
@@ -4,7 +4,9 @@ namespace app\common\tools\phpparser;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Identifier;
 use PhpParser\Node\Stmt;
+use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Global_;
 use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitorAbstract;
@@ -26,6 +28,17 @@ class NodeFakeVarVisitorTools extends NodeVisitorAbstract
         '_SESSION',
         'this'
     ];
+
+    public function enterNode(Node $node)
+    {
+        if ($node instanceof ClassMethod) {
+            if ($node->name instanceof Identifier) {
+                if ($node->name->name == '__construct') {
+                    return NodeTraverser::DONT_TRAVERSE_CHILDREN;
+                }
+            }
+        }
+    }
 
     public function leaveNode(Node $node)
     {
