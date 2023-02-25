a358cc4ae9b1522c037020a229da38c17211588a
augushong <augushong@ulthon.com>
2023-02-25 09:48:48 +0800

    支持关闭layui的toobar

--- public/static/plugs/easy-admin/easy-admin.js
+++ public/static/plugs/easy-admin/easy-admin.js
@@ -261,12 +261,14 @@ define(["jquery", "tableSelect", "ckeditor", 'miniTheme', 'tableData', 'citypick
                 options.limit = options.limit || 15;
                 options.limits = options.limits || [10, 15, 20, 25, 50, 100];
                 options.cols = options.cols || [];
-                options.defaultToolbar = (options.defaultToolbar === undefined && !options.search) ? ['filter', 'print'] : ['filter', 'print', {
-                    title: '搜索',
-                    layEvent: 'TABLE_SEARCH',
-                    icon: 'layui-icon-search',
-                    extend: 'data-table-id="' + options.id + '"'
-                }];
+                if (options.defaultToolbar !== false) {
+                    options.defaultToolbar = (options.defaultToolbar === undefined && !options.search) ? ['filter', 'print'] : ['filter', 'print', {
+                        title: '搜索',
+                        layEvent: 'TABLE_SEARCH',
+                        icon: 'layui-icon-search',
+                        extend: 'data-table-id="' + options.id + '"'
+                    }];
+                }
 
                 var table2card = function () {
 
