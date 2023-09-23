dd6f33ffc40c1e9a6ba7512388ecdf3c025e6fc2
augushong <augushong@ulthon.com>
2023-09-23 18:06:17 +0800

    发布新版本

--- app/common/command/admin/Version.php
+++ app/common/command/admin/Version.php
@@ -12,12 +12,13 @@ use think\console\Output;
 
 class Version extends Command
 {
-    public const VERSION = 'v2.0.40';
+    public const VERSION = 'v2.0.41';
 
     public const LAYUI_VERSION = '2.8.16';
 
     public const COMMENT = [
-        '删除事件代码',
+        '调整主要代码的扩展方式；',
+        '调整更新逻辑',
         '发布新版本',
     ];
 
