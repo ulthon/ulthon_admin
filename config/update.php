3dcf336bbc24a751044c698db0b705082e4a8ae6
augushong <augushong@ulthon.com>
2023-09-25 11:12:57 +0800

    修改著作信息；修改更新配置文件

--- config/update.php
+++ config/update.php
@@ -8,6 +8,7 @@ $skip_files[] = 'app/common/app/functions.php';
 $skip_files[] = 'app/common/app/listen.php';
 $skip_files[] = 'app/common/app/middleware.php';
 $skip_files[] = 'app/common/app/service.php';
+$skip_files[] = 'app/common/app/provider.php';
 $skip_files[] = 'composer.lock';
 $skip_files[] = 'README.md';
 $skip_files[] = 'README.en.md';
@@ -29,7 +30,14 @@ $config['skip_dir'] = $skip_dir;
 
 // append 如果当前版本不存在，则追加，如果存在，则不应当覆盖
 // append 的文件应当在skip内部
-$config['append_files'] = [];
+
+$append_files = [];
+$append_files[] = 'app/common/app/functions.php';
+$append_files[] = 'app/common/app/listen.php';
+$append_files[] = 'app/common/app/middleware.php';
+$append_files[] = 'app/common/app/service.php';
+$append_files[] = 'app/common/app/provider.php';
+$config['append_files'] = $append_files;
 
 $append_dir = [];
 $append_dir[] = 'app/admin/controller';
