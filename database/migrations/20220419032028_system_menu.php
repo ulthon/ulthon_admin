daae12610e75333aa2ce6aa9d4e60c1e54e1f87e
augushong <augushong@ulthon.com>
2023-09-28 17:23:48 +0800

    修改默认字段

--- database/migrations/20220419032028_system_menu.php
+++ database/migrations/20220419032028_system_menu.php
@@ -35,7 +35,7 @@ class SystemMenu extends Migrator
             ->addColumn(Column::char('icon', 100)->setDefault('')->setComment('菜单图标'))
             ->addColumn(Column::char('href', 100)->setDefault('')->setComment('链接'))
             ->addColumn(Column::char('auth_node', 100)->setDefault('')->setComment('权限标志'))
-            ->addColumn(Column::text('params')->setDefault('')->setComment('链接参数'))
+            ->addColumn(Column::text('params')->setNullable()->setComment('链接参数'))
             ->addColumn(Column::char('target', 20)->setDefault('_self')->setComment('链接打开方式'))
             ->addColumn(Column::integer('sort')->setDefault(0)->setComment('排序'))
             ->addColumn(Column::tinyInteger('status')->setLimit(1)->setDefault(1)->setComment('状态 {radio} (0:禁用,1:启用)'))
